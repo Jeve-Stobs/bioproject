3e8669ee63837dc48b0a2da704cda2e99686db85
Allegedly Samuel Casey <76224164+Jeve-Stobs@users.noreply.github.com>
2021-05-25 10:49:33 -0400

    Add files via upload

--- mySketch.js
+++ mySketch.js
@@ -4,55 +4,66 @@ var gravity = 1;
 var jump = 15;
 var speed = 1;
 let x = 40;
+var bgImg;
+var x1 = 0;
+var x2;
+
+var scrollSpeed = 1;
+
 function setup() {
-	createCanvas(600, 600);
+	createCanvas(windowWidth, windowHeight);
 	background(100);
-	character = createSprite(300,100);
+	character = createSprite(300, 100);
 	var anim = character.addAnimation('idle', 'stickman.png');
-	
+	x2 = width;
+
 	Grass = new Group();
-	
-	for (let i=0; i<21; i++) {
-		var newTile = createSprite(x,550);
+
+	for (let i = 0; i < 21; i++) {
+		ycord = random(1, 600)
+		var newTile = createSprite(x, ycord);
 		newTile.addAnimation('normal', 'grass.png');
 		newTile.addToGroup(Grass);
 		newTile.debug = mouseIsPressed;
-		x+=80;
+		x += 80;
 	}
 }
 
+function preload() {
+	bgImg = loadImage("bg.jpg");
+}
+
+
 function draw() {
-	background(100);
 	character.collide(Grass);
 	Grass.displace(character);
-	Grass[0].debug = mouseIsPressed;
-	Grass[1].debug = mouseIsPressed;
-	Grass[2].debug = mouseIsPressed;
-	Grass[3].debug = mouseIsPressed;
-	Grass[4].debug = mouseIsPressed;
-	Grass[5].debug = mouseIsPressed;
-	Grass[6].debug = mouseIsPressed;
-	Grass[7].debug = mouseIsPressed;
-	Grass[8].debug = mouseIsPressed;
-	Grass[9].debug = mouseIsPressed;
-	Grass[10].debug = mouseIsPressed;
-	character.debug = mouseIsPressed;
+	image(bgImg, x1, 0, width, height);
+	image(bgImg, x2, 0, width, height);
+	x1 -= scrollSpeed;
+	x2 -= scrollSpeed;
+
+	if (x1 < -width) {
+		x1 = width;
+	}
+	if (x2 < -width) {
+		x2 = width;
+	}
 	character.velocity.y += gravity;
-	
-	if (character.position.x>window.width) {
+	character.velocityY = character.velocityY + 0.5;
+
+	if (character.position.x > window.width) {
 		character.bounce = true;
 	}
 	if (keyIsPressed) {
-	if (key == 'a') {
+		if (key == 'a') {
 			character.velocity.x -= speed;
-		}else if (key == 'd') {
-		character.velocity.x += speed;
-		}else if (key == 'w'&&character.position.y>460) {
-		character.velocity.y = -jump;
-		character.rotate = 11;
+		} else if (key == 'd') {
+			character.velocity.x += speed;
+		} else if (key == 'w') {
+			character.velocity.y = -jump;
 		}
 	}
 	character.maxSpeed = 10;
-	character.setCollider('circle',0,0,20);
+	character.setCollider('circle', 0, 0, 20);
 	drawSprites();
 }
